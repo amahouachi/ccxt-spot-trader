f7703e450d4ee768e20370a31f2f536dad18713e
Ahmed Mahouachi <levieux@gmail.com>
2024-11-09 13:26:09 +0100

    remove .js

--- src/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OrderSide = void 0;
-var OrderSide;
-(function (OrderSide) {
-    OrderSide["buy"] = "buy";
-    OrderSide["sell"] = "sell";
-})(OrderSide || (exports.OrderSide = OrderSide = {}));
